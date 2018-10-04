bd527ede91d64bc94db974453ccb6ee4fb708eb3
chrisesquerra <esquerra>
2018-10-04 11:39:59 -0500

    where clause exercise part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,7 @@
 use employees;
 
-select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
-select * from employees where last_name like 'E%';
-select * from employees where hire_date >'1989-12-31' and hire_date < '200-01-01';
-select * from employees where birth_date like '%12-25%';
-select * from employees where last_name like '%q%';
+select * from employees where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+select * from employees where last_name like 'E%' or last_name like '%e';
+select * from employees where last_name like 'E%e';
+select * from employees where hire_date >'1989-12-31' and hire_date < '2000-01-01' and birth_date like '%12-25%';
+select * from employees where last_name like '%q%' and last_name not like '%qu%';
